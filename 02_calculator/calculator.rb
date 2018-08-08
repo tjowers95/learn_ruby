84dc6e0ea8e4e2b275dbc35f976a2a960616d2d7
Tyler Jowers <Jowers.tc@protonmail.com>
2018-08-07 22:31:39 -0400

    03 done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
-#write your code here
+def add(w,v)
+  return w + v
+end
+def subtract(w,v)
+  return w - v
+end
+def sum(a)
+  w = 0
+  a.each do |v|
+    w += v
+  end
+  return w
+end
+def multiply(*args)
+  w = 1
+  args.each do |v|
+    w *= v
+  end
+  return w
+end
+def power(n,x)
+  a = n
+  (x-1).times{n *= a}
+  return n
+end
+def factorial(n)
+    (1..n).reduce(:*) || 1
+end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,48 @@
-#write your code here
+def echo(s)
+  return s
+end
+def shout(s)
+  return s.upcase
+end
+def repeat(s,x=nil)
+  x ||= 2
+  w = ""
+  x.times {w << s}
+  a = w.gsub(/#{s}/,"#{s} ")
+  a.chop!
+  return a
+end
+def start_of_word(s,i)
+  c = 1
+  ss = ''
+  while (c <= i) do
+    ss << s[c-1]
+    c += 1
+  end
+  return ss
+end
+def first_word(s)
+ i = 0
+ ss = ''
+ while (i < s.index(' ')) do
+   ss << s[i]
+   i += 1
+ end
+ return ss
+end
+def titleize(s)
+  a = s.split(/ /)
+  i = 0
+  q = ''
+  while (i < a.length) do
+    if (a[i].length > 4 || i == 0 || i == (a.length - 1))
+      q << a[i].capitalize
+      q << ' '
+    else
+    q << a[i]
+    q << ' '
+    end
+    i += 1
+  end
+  return q.chop!
+end
